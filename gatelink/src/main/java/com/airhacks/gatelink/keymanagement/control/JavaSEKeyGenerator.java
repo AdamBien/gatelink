ce17946ea54215e5dd475cfc7ae6efb8af7eac09
Adam Bien <abien@adam-bien.com>
2024-01-10 16:10:06 +0100

    EC parameter set

--- gatelink/src/main/java/com/airhacks/gatelink/keymanagement/control/JavaSEKeyGenerator.java
+++ gatelink/src/main/java/com/airhacks/gatelink/keymanagement/control/JavaSEKeyGenerator.java
@@ -1,9 +1,11 @@
 package com.airhacks.gatelink.keymanagement.control;
 
+import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
 
 import com.airhacks.gatelink.keymanagement.boundary.KeyGenerationException;
 import com.airhacks.gatelink.keymanagement.entity.JavaSEServerKeys;
@@ -13,15 +15,16 @@ public class JavaSEKeyGenerator {
     public JavaSEServerKeys generateVapidKeys() {
 
         try {
+            var parameterSpec = new ECGenParameterSpec("secp256r1");
             var keyPairGenerator = KeyPairGenerator.getInstance("EC");
 
-            keyPairGenerator.initialize(256);
+            keyPairGenerator.initialize(parameterSpec);
             var serverKey = keyPairGenerator.generateKeyPair();
             var privateKey = (ECPrivateKey) serverKey.getPrivate();
             var publicKey = (ECPublicKey) serverKey.getPublic();
             return new JavaSEServerKeys(privateKey, publicKey);
 
-        } catch ( NoSuchAlgorithmException  ex) {
+        } catch ( NoSuchAlgorithmException | InvalidAlgorithmParameterException  ex) {
             throw new KeyGenerationException(ex.getMessage());
         }
     }    
