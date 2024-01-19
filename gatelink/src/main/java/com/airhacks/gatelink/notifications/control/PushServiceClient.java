741d1dd4bc3c3b5ab5b5b15b905a9e8241b963ab
Adam Bien <abien@adam-bien.com>
2024-01-19 06:38:55 +0100

    media type / try-with-resources

--- gatelink/src/main/java/com/airhacks/gatelink/notifications/control/PushServiceClient.java
+++ gatelink/src/main/java/com/airhacks/gatelink/notifications/control/PushServiceClient.java
@@ -1,26 +1,30 @@
 package com.airhacks.gatelink.notifications.control;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.http.*;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;;
 
 public interface PushServiceClient {
 
-    
-    public static HttpResponse<Void> sendNotification(String endpoint, String salt, String ephemeralPublicKey, String vapidPublicKey, String authorizationToken, byte[] encryptedContent) 
-    throws IOException, InterruptedException{
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest
-        .newBuilder()
-        .POST(BodyPublishers.ofByteArray(encryptedContent))
-        .header("TTL", "2419200")
-        .header("Content-Encoding", "aesgcm")
-        .header("Encryption", "salt=" + salt)
-        .header("Authorization", "WebPush " + authorizationToken)
-        .header("Crypto-Key", "dh=" + ephemeralPublicKey + ";p256ecdsa=" + vapidPublicKey)
-        .build();
-        return client.send(request, BodyHandlers.discarding());
+    public static HttpResponse<Void> sendNotification(String endpoint, String salt, String ephemeralPublicKey,
+            String vapidPublicKey, String authorizationToken, byte[] encryptedContent)
+            throws IOException, InterruptedException {
+        var uri = URI.create(endpoint);
+        try (var client = HttpClient.newHttpClient()) {
+            var request = HttpRequest
+                    .newBuilder(uri)
+                    .POST(BodyPublishers.ofByteArray(encryptedContent))
+                    .header("TTL", "2419200")
+                    .header("Content-Encoding", "aesgcm")
+                    .header("Encryption", "salt=" + salt)
+                    .header("Authorization", "WebPush " + authorizationToken)
+                    .header("Crypto-Key", "dh=" + ephemeralPublicKey + ";p256ecdsa=" + vapidPublicKey)
+                    .header("Content-type", "application/octet-stream")
+                    .build();
+            return client.send(request, BodyHandlers.discarding());
+        }
     }
 
 }
