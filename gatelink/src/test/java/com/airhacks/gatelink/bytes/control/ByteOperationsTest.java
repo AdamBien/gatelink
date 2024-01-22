1eefa8718b32ced462fea02505f035d52c6a1790
Adam Bien <abien@adam-bien.com>
2024-01-22 18:38:32 +0100

    UT for concat

--- gatelink/src/test/java/com/airhacks/gatelink/bytes/control/ByteOperationsTest.java
+++ gatelink/src/test/java/com/airhacks/gatelink/bytes/control/ByteOperationsTest.java
@@ -3,11 +3,9 @@ package com.airhacks.gatelink.bytes.control;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.math.BigDecimal;
-
 import org.junit.jupiter.api.Test;
 
-public class BytesTest {
+public class ByteOperationsTest {
     @Test
     void unsignedIntToBytes() {
         byte result[] = ByteOperations.unsignedIntToBytes(42);
@@ -30,5 +28,17 @@ public class BytesTest {
         var oneF = ByteOperations.parseHex("F");
         var decimalRepresentation = Byte.valueOf(oneF[0]).intValue();
         assertThat(decimalRepresentation).isEqualTo(15);
+    }
+
+    @Test
+    void concat(){
+        byte first[] = {0,1};
+        byte second[] = {2,3};
+        var result = ByteOperations.concat(first,second);
+        assertThat(result).hasSize(4);
+        assertThat(result[0]).isEqualTo((byte)0);
+        assertThat(result[1]).isEqualTo((byte)1);
+        assertThat(result[2]).isEqualTo((byte)2);
+        assertThat(result[3]).isEqualTo((byte)3);
     }
 }
