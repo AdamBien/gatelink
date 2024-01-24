0217c72998ee6a71ea5ea2b34c71c1cf162430fb
Adam Bien <abien@adam-bien.com>
2024-01-24 07:15:50 +0100

    javadoc improvements and refactoring

--- gatelink/src/main/java/com/airhacks/gatelink/encryption/control/AESEncryptor.java
+++ gatelink/src/main/java/com/airhacks/gatelink/encryption/control/AESEncryptor.java
@@ -14,16 +14,29 @@ import javax.crypto.spec.SecretKeySpec;
 
 import com.airhacks.gatelink.bytes.control.ByteOperations;
 
+/**
+ * AES encryption with GCM mode
+ * https://www.ietf.org/rfc/rfc5116.txt
+ * 
+ * Recommendation for Block
+ * Cipher Modes of Operation:
+ * Galois/Counter Mode (GCM)
+ * and GMAC:
+ * https://csrc.nist.gov/pubs/sp/800/38/d/final
+ * (https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf)
+ */
 public interface AESEncryptor {
 
-    int TAG_SIZE = 16;
+    int AUTHENTICATION_TAG_LENGTH = 16;
 
-
+    /**
+     * Encrypts the content with AES in Galois/Counter Mode (GCM) mode
+     */
     static byte[] encryptWithAES(byte[] key, byte[] content, byte[] nonce)
             throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException,
             InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
         var cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        var params = new GCMParameterSpec(TAG_SIZE * 8, nonce);
+        var params = new GCMParameterSpec(AUTHENTICATION_TAG_LENGTH * 8, nonce);
         cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "AES"), params);
         var twoBytes = cipher.update(new byte[2]);
         var encryptedMessage = cipher.doFinal(content);
